cb402ba3c2ce799e9042865038e44f5b8716feab
John Fashanu <adejun4life@yahoo.com>
2023-07-21 01:45:47 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Program that prints all single digit numbers starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
