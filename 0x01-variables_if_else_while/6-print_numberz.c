f82685c85211d5710896ceafcc04307a3e16c8ab
John Fashanu <adejun4life@yahoo.com>
2023-07-21 02:02:25 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 /**
  * main - Program that prints all single digit numbers starting from 0
  *
@@ -7,10 +9,10 @@
 
 int main(void)
 {
-	int n;
+	int d;
 
-	for (n = 0; n < 10; n++)
-		putchar(n);
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
 	putchar('\n');
 	return (0);
 }
