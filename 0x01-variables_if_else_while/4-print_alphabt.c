fcc21ad2483da7248a0249a0c71acd8a18bc9626
John Fashanu <adejun4life@yahoo.com>
2023-07-21 01:23:08 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Program that prints the alphabet in lowercase except q amd e
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a';
+	
+	while (c <= 'z')
+	{
+		if (c != 'q' && c = 'e')
+		{
+			putchar(c);
+		}
+		c++;
+	}
+	putchar(c);
+	return (0);
+}
+
+
+
